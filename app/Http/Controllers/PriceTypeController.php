4d771c79f904863dfc20aac7d00b246c7acdfbc9
Hnin Yu Shwe <hninyushwe1530@gmail.com>
2025-01-04 21:25:02 +0630

    add-roomPrice

--- app/Http/Controllers/PriceTypeController.php
+++ app/Http/Controllers/PriceTypeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PriceType;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Str;
 
 class PriceTypeController extends Controller
 {
@@ -13,7 +16,10 @@ class PriceTypeController extends Controller
      */
     public function index()
     {
-        //
+        $price_types = PriceType::all();
+        return view('admin.price-types.price-type',['price_types'=>$price_types]);
+
+        // return view('admin.room-types.create-room-types',['price_types' => $price_types]);
     }
 
     /**
@@ -23,7 +29,7 @@ class PriceTypeController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.price-types.create-price-type');
     }
 
     /**
@@ -34,7 +40,23 @@ class PriceTypeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+        ],[
+            'name.required' => 'Price Type is required'
+        ]);
+        //character change to lowercase and remove all extra blank space
+        $updateName = str(Str::lower($request->name))->squish();
+        $priceTypes = PriceType::all();
+        // Check price type is already exit or not.
+        foreach($priceTypes as $priceType){
+            if($priceType->name == $updateName){
+                return redirect()->route('price-types.create')->with('fail','Price Type is already exit.');
+            }
+        }
+        PriceType::create(['name'=>$updateName]);
+
+        return redirect()->route('price-types.index')->with('success','Price Type is successfully created');
     }
 
     /**
@@ -54,9 +76,9 @@ class PriceTypeController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(PriceType $priceType)
     {
-        //
+        return view('admin.price-types.edit-price-type',["priceType"=>$priceType]);
     }
 
     /**
@@ -68,7 +90,30 @@ class PriceTypeController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+        ],[
+            'name.required' => 'Price Type is required'
+        ]);
+
+        //character change to lowercase and remove all extra blank space
+        $updateName = str(Str::lower($request->name))->squish();
+
+        $priceType = PriceType::find($id);
+
+        $priceTypes = PriceType::all();
+
+        if($priceType->name != $updateName){
+        // Check price type is already exit or not.
+        foreach($priceTypes as $price_type){
+            if($price_type->name == $updateName){
+                return redirect()->route('price-types.edit', $priceType)->with('fail','Price Type is already exit.');
+            }
+        }
+        }
+        $priceType->name = $updateName;
+        $priceType->save();
+        return redirect()->route('price-types.index')->with('success','Price Type is successfully updated');
     }
 
     /**
@@ -79,6 +124,14 @@ class PriceTypeController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $priceType = PriceType::find($id);
+        try{
+            $priceType->delete();
+        }
+        catch(QueryException $e){
+            return redirect()->route('price-types.index')->with(["fail"=>"Price Type can't be deleted ."]);
+        }
+        return redirect()->route('price-types.index')->with(["success"=>"Price Type is successfully deleted."]);
+
     }
 }
