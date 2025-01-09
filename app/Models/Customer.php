c9d57bc71c3977662a66ed56129f96b1a3dbd656
ninewaiyan <ninewaiyan2022@gmail.com>
2025-01-09 23:10:31 +0630

    update

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Customer extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'status'
+    ];
+
+    protected $hidden = [
+        'password',
+        
+    ];
+
+    public function bookings(){
+        return $this->hasMany(Booking::class);
+    }
+    
 }
