b0f1e4d0edb6c2e1e1e2b4fd4370c8f7c352eba6
yuthadda <yuthadda22@gmail.com>
2025-02-03 12:40:14 +0630

    route add

--- routes/web.php
+++ routes/web.php
@@ -42,8 +42,19 @@ Route::group(['namespace'=>'App\Http\Controllers'],function(){
     Route::post('/verify-otp',[AuthController::class,'verifyOtp']);
     Route::post('/verify-forgot-otp',[AuthController::class,'verifyForgotOtp'])->name('verifyforgot.otp');
 
+    Route::get('/promotion',[UIController::class,'promotion']);
+    Route::post('/search',[UIController::class,'search']);
+    Route::post('/booking',[UIController::class,'booking']);
+    Route::post('/bookingform',[UIController::class,'bookingform']);
+    Route::post('/storebooking',[UIController::class,'storebooking']);
+    Route::get('/history/{id}',[UIController::class,'history']);
+    Route::get('/viewprofile/{id}',[UIController::class,'viewprofile']);
+    Route::post('/updateprofile',[UIController::class,'updateprofile']);
+    Route::get('/editprofile/{id}',[UIController::class,'editprofile']);
+    Route::get('/updateprofile',[UIController::class,'updateprofile']);
+    Route::post('/verifyotp',[UIController::class,'verifyOtp']);
+    Route::get('/changepassword/{id}',[UIController::class,'changepassword']);
+    Route::post('/updatepassword',[UIController::class,'updatepassword']);
 
-
-  
 
 });
